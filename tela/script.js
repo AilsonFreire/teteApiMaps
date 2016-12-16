566a8727c20efae19ab9b26125bc3cb29548d7d4
Ailson Rocha Freire <ailson_freire@outlook.com>
2016-12-16 15:56:54 -0200

    v1
    separação dos elementos

---
+++ tela/script.js
@@ -0,0 +1,12 @@
+var map;
+function testeMap() {
+	
+	alert('woow');
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -1.4558333333, lng: -48.5027777778},
+    zoom: 15
+  });
+//  console.log(map);
+}
+
+  
