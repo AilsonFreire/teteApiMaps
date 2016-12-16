89ed4f9ec6a799d09ecf4b7c8b53361c51f57e99
Ailson Rocha Freire <ailson_freire@outlook.com>
2016-12-16 16:52:00 -0200

    v2
    mudança

--- tela/script.js
+++ tela/script.js
@@ -1,12 +1,20 @@
-var map;
-function testeMap() {
-	
-	alert('woow');
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -1.4558333333, lng: -48.5027777778},
-    zoom: 15
-  });
-//  console.log(map);
-}
 
-  
+function createMap() {
+	var map;
+	var mapOptions = {
+		center: new google.maps.LatLng(-1.4558333333, -48.5027777778),
+		zoom: 15, 
+	};
+	
+	map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	console.log(map)
+};
+
+function loadTheMap(){
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD1qCeTpnDCkDd1veNtsBUe2BEpw-o4c4s&callback=createMap&libraries=drawing,visualization,geometry&amp;sensor=false&amp;language=pt&amp;region=BR';
+	document.body.appendChild(script);
+};
+
+//window.onload = loadTheMap;
